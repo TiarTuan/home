9a85f1f46cd374b43e9086ae13d9a8036c8bd5a0
TiarTuan <95533050+TiarTuan@users.noreply.github.com>
2022-08-10 22:26:28 +0200

    Add files via upload

---
+++ PYTHON/home.py
@@ -0,0 +1,18 @@
+import turtle
+
+home = turtle.Turtle()
+home.pencolor("#ffba00")
+
+home.forward(150)
+home.right(90)
+home.forward(150)
+home.right(90)
+home.forward(150)
+home.right(90)
+home.forward(150)
+home.right(32)
+home.forward(150)
+home.right(120)
+home.forward(151)
+
+turtle.done()
